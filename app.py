1b59c04abab9acbd32ae11d4407d8be2c34fd4c5
vinay jain <vinayjain404@gmail.com>
2014-07-20 23:14:45 -0700

    add a v1 api structure to the application

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 # Blackjack application
 from flask import Flask
+
+from constants import SECRET_KEY
+
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
+
 
 @app.route("/")
-def hello():
+def home():
+    return "Lets play Blackjack"
+
+
+@app.route("/deal")
+def deal():
     return "Hello World!"
+
+
+@app.route("/hit")
+def hit():
+    return "Hello World!"
+
+
+@app.route("/stand")
+def stand():
+    return "Hello World!"
+
+
+@app.route("/cards")
+def cards():
+    return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run()
