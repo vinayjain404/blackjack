d52725b926c0063c1dfd24a39f91ebea47f71d9c
vinay jain <vinayjain404@gmail.com>
2014-07-21 00:20:32 -0700

    add number and suit options to the constants

--- constants.py
+++ constants.py
@@ -1,2 +1,25 @@
+NUMBER_CHOICES = (
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+)
+
+SUIT_CHOICES = (
+    "HEARTS",
+    "SPADES",
+    "DIAMONDS",
+    "CLUBS",
+)
+
 # TODO vinayjain Move this out of code and version control
-SECRET_KEY = ',tp\xa0\xe1\xe5\xfc\x8b\xaa\xfd\x81;\xbe\x16\x153\x1d\x19f\xf5@Y\xf9J'
+SECRET_KEY = ",tp\xa0\xe1\xe5\xfc\x8b\xaa\xfd\x81;\xbe\x16\x153\x1d\x19f\xf5@Y\xf9J"
